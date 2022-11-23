5dab5f7fc57589467633ee6642d174cf07e357d6
painful_pig <576772193@qq.com>
2022-11-23 15:26:02 +0800

    测试init版本信息

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,3 +1,8 @@
+/**
+ * 初始化
+ * @param {String} projectName
+ * @param {Object} options
+ */
 function init(projectName, options) {
   // 通过环境变量获取传入的 targetPath
   console.log('init', projectName, options, process.env.CLI_TARGET_PATH);
